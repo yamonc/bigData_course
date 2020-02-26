837dc3abecc7b246f1c45eb39d54b92a5bef03b0
yamon <lovechen1996428@qq.com>
2020-02-26 23:10:12 +0800

    元组字典列表字符串 python类的学习 python类的练习题完成

---
+++ base_python/0215/Clock.py
@@ -0,0 +1,43 @@
+from time import sleep
+
+
+class Clock(object):
+    """数字时钟"""
+
+    def __init__(self, hour=0, minute=0, second=0):
+        """初始化方法
+        :param hour:时
+        :param minute:分
+        :param second:秒
+        """
+        self._hour = hour
+        self._minute = minute
+        self._seconde = second
+
+    def run(self):
+        """走字"""
+        self._seconde += 1
+        if self._seconde == 60:
+            self._seconde = 0
+            self._minute += 1
+            if self._minute == 60:
+                self._minute = 0
+                self._hour += 1
+                if self._hour == 24:
+                    self._hour = 0
+
+    def show(self):
+        """显示时间"""
+        return '%02d:%02d:%02d' % (self._hour, self._minute, self._seconde)
+
+
+def main():
+    clock = Clock(23, 59, 58)
+    while True:
+        print(clock.show())
+        sleep(1)
+        clock.run()
+
+
+if __name__ == '__main__':
+    main()
